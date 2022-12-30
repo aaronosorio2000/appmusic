e8156e8e41eda692d557fed41b0dd1b986c4f37b
Aaron Osorio <aaronosorio2000@gmail.com>
2022-12-30 01:22:03 -0500

    appmusic

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const ShazamCoreApi = createApi({
+  reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        "fe39d60025mshae60758850211b0p1d5d01jsn0ff2358cdb84"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => "/v1/charts/world" }),
+    getSongsByGenre: builder.query({
+      query: (genre) => `/v1/charts/genre-world?genre_code=${genre}`,
+    }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/v1/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `/v1/tracks/related?track_id=${songid}`,
+    }),
+    getArtistDetails: builder.query({
+      query: (artistId) => `/v2/artists/details?artist_id=${artistId}`,
+    }),
+    getSongBySearch: builder.query({
+      query: (searchTerm) =>
+        `/v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+    }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongBySearchQuery,
+} = ShazamCoreApi;
